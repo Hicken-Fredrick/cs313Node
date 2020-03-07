d55a74af2c9d1120b25360b9e430d53d68260a42
Fred Hicken <35247195+Melrium@users.noreply.github.com>
2020-03-07 11:54:53 -0700

    testing

--- index.js
+++ index.js
@@ -7,9 +7,20 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/USPSCalc'))
-  .get('/getRate', (req, res) => res.render('pages/USPSCalcResult'))
+  .post('/getRate', (req, res) => {
+    var data = $("#calcRates :input").serializeArray();
+    console.log(data);
+    res.render('pages/USPSCalcResult');
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 function calcRate(weight, type) {
   
 }
+
+.post('/math', (req, res) => {
+        var data = req.body;
+        var result = math(Number(data.num1), Number(data.num2), data.operator);
+        console.log(result);
+        res.render('pages/result', { result: result });
+    })

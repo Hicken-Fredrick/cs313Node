37138114df1ae7bafe808f91511a8f4c9514dc1e
Fred Hicken <35247195+Melrium@users.noreply.github.com>
2020-03-07 13:20:03 -0700

    testing

--- index.js
+++ index.js
@@ -1,24 +1,70 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+var bodyParser = require('body-parser')
+ 
 
 express()
+  .use( bodyParser.json() )
+  .use(bodyParser.urlencoded({     
+  extended: true
+  }))
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/USPSCalc'))
   .post('/getRate', (req, res) => {
-    res.render('pages/USPSCalcResult');
-  });
+    var weight = req.body.weight;
+    var type = req.body.type;
+    var result = calcRate(Number(weight), type);
+    res.render('pages/USPSCalcResult', { result: result });
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 function calcRate(weight, type) {
-  
+  costWeight = Math.round(weight);
+  switch(type) {
+    case'letterStamp':
+      if (costWeight < 1) {
+        return .55; 
+      } else if (costWeight < 5) {
+        return .4 + (costWeight * .15); 
+      } else {
+        return "TOO HEAVY!"; 
+      }
+      break;
+    case'letterMeter':
+      if (costWeight < 1) {
+        return .50; 
+      } else if (costWeight < 5) {
+        return .35 + (costWeight * .15); 
+      } else {
+        return "TOO HEAVY!";
+      }
+      break;
+    case'largeEnvelope':
+      if (costWeight < 1) {
+        return 1; 
+      } else if (costWeight < 14) {
+        return 1 + (costWeight * .2); 
+      } else {
+        return "TOO HEAVY!";
+      }
+      break;
+    case'package':
+      if (costWeight < 5) {
+        return 3.80; 
+      } else if (costWeight < 9) {
+        return 4.60; 
+      } else if (costWeight < 13){
+        return 5.30; 
+      } else if (costWeight < 14) {
+        return 5.90; 
+      } else {
+        return "TOO HEAVY!"; 
+      }
+      break;
+    default:
+      
+  }
 }
-
-.post('/math', (req, res) => {
-        var data = req.body;
-        var result = math(Number(data.num1), Number(data.num2), data.operator);
-        console.log(result);
-        res.render('pages/result', { result: result });
-    })
